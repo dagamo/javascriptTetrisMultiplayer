75c2d7e3293cc8fa9be57b4a83ac802aa63f22d2
David <david.garcia@hummingbirdit.tech>
2019-06-25 15:19:34 -0600

    changes

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3000');
+var socket = io('https://tetris-multiplayer-real-time.herokuapp.com/');
 
 var params = new URLSearchParams(window.location.search);
 console.log('Params', params);
